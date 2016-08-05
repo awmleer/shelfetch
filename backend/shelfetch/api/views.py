7b5ae62e6fce45398ee9f6974df08bf77ae7a18d
haoguangbo <haoguangbo@yeah.net>
2016-08-05 09:59:15 +0800

    django init

--- backend/shelfetch/api/views.py
+++ backend/shelfetch/api/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import authenticate #用户登录认证
+from django.contrib.auth.decorators import login_required,permission_required
+import json
+import logging
+from django.shortcuts import redirect
 
+# Get an instance of a logger
+logger = logging.getLogger('django')
 # Create your views here.
+
+
+
+
+def test(request):
+    res={
+        "test":'ok'
+    }
+    return HttpResponse(json.dumps(res), content_type="application/json")
+
